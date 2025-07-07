a3a13b9ff6a8a6320abb3ab0ec88135301743b22
tati0906 <tatianavanegas2006@gmail.com>
2025-07-07 16:31:26 -0500

    feat: estados de carga y mensajes de espera con useState

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a Todo App</h1>
+      <p>Una aplicación simple para gestionar tus tareas.</p>
+    </div>
+  );
+}
+
+export default Home;
