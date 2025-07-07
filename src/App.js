aa559aedc58d989e8fdccac61b1a356b00982138
tati0906 <tatianavanegas2006@gmail.com>
2025-07-07 16:31:07 -0500

    feat: función para eliminar todos del listado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import EditTodo from './components/EditTodo'; // <--- Nueva importación
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navegación simple */}
+        <nav>
+          <Link to="/">Inicio</Link>
+          <Link to="/todos">Mis Todos</Link>
+          <Link to="/add">Agregar Todo</Link>
+        </nav>
+
+        {/* Rutas */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todos" element={<TodoList />} />
+          <Route path="/add" element={<AddTodo />} />
+          <Route path="/edit/:id" element={<EditTodo />} /> {/* <--- Nueva ruta */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
